6e093f8804612d19816acc3d1344d73864a1172d
jack_daniel <134845344+gistjackdaniel@users.noreply.github.com>
2025-03-22 05:47:30 +0000

    버그 수정 및 코드 최적화

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -5,8 +5,8 @@ public class JavaExercises {
 
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
-        // TODO: Fill in this function.
-        return null;
+        int[] diceArray = {1, 2, 3, 4, 5, 6};
+        return diceArray;
     }
 
     /** Returns the order depending on the customer.
@@ -14,15 +14,33 @@ public class JavaExercises {
      *  If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
-        // TODO: Fill in this function.
-        return null;
+        if (customer.equals("Ergun")) 
+        {
+            return new String[] { "beyti", "pizza", "hamburger", "tea" };
+        }
+        else if (customer.equals("Erik")) 
+        {
+            return new String[] { "sushi", "pasta", "avocado", "coffee" };
+        } else return new String [3];
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
-        // TODO: Fill in this function.
-        return 0;
+      int min = array[0];
+      for (int i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i]; 
+        }
+      }
+      int max = array[0];
+      for (int j = 0; j < array.length; j++) {
+        if (array[j] > max) {
+            max = array[j];
+        }
+      }
+      int diff = max - min;
+    return diff;  
     }
 
     /**
@@ -38,8 +56,42 @@ public class JavaExercises {
     }
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
-        // TODO: Fill in this function.
-        return null;
+        list.add(x);
+        if (x==1){
+            return list;
+        }
+        if(x % 2 == 0){
+          return hailstoneHelper(x/2, list);
+        }
+        else return hailstoneHelper((x * 3) + 1, list);
+    }
+
+    public static void main(String[] args){
+        // case 1
+        int[] dice = makeDice();
+        for (int i = 0; i < dice.length; i++) {
+            System.out.print(dice[i]+ " ");
+        }
+        
+        //case 2
+        String[] orderForErgun = takeOrder("Ergun");
+        for (String item : orderForErgun) {
+            System.out.println(item);
+        }
+
+        //case 3
+        int[] array = {1, 2, 3, 4 ,5};
+        int result = findMinMax(array);
+        System.out.println(result); 
+
+
+        // case 4
+        List<Integer> sequence = hailstone(7);
+        System.out.println(sequence); 
+
     }
 
 }
+
+
+
